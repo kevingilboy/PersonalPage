ebe4d5873e2b3957f371d95154debcc19213490d
Kevin Gilboy <kevinmgilboy@gmail.com>
2016-02-05 16:38:36 -0500

    Added tooltips for weather
    Hovering over the weather shows a 5 day forecast

--- weather.js
+++ weather.js
@@ -10,10 +10,9 @@
  */
 
 function getWeather() {
-	//Fetch stock info in JSON format
-	var url = 'http://api.openweathermap.org/data/2.5/weather?q=Pittsburgh&appid=44db6a862fba0b067b1930da0d769e98';
+	//Fetch current weather info in JSON format
+	var url = 'http://api.openweathermap.org/data/2.5/weather?id=5206379&appid=3aa8d2b200ad314410e9340650ad4140';
 	$.getJSON(url, function(weather){
-		var description = weather.weather[0].main;
 		var tHigh = Math.round(1.8*(weather.main.temp_max-273.15)+32);
 		var tLow = Math.round(1.8*(weather.main.temp_min-273.15)+32);
 		var t = Math.round(1.8*(weather.main.temp-273.15)+32);
@@ -23,5 +22,37 @@ function getWeather() {
 		document.getElementById('weather').innerHTML += '<font style="font-size:2em; margin-right:10px;">'+t+'&deg</font>';
 		document.getElementById('weather').innerHTML += '<font color=\"#ff4d4d\" style=\"vertical-align:.8em\">'+tHigh+'</font><font style=\"vertical-align:.5em\">&frasl;</font><font color=\"#4d88ff\" style=\"vertical-align:0em\">'+tLow+'</font>';
 		document.getElementById('weather').innerHTML += '<img src=\"' + icon + '\" style=\"position:absolute; top:12; margin-left:10px\">';
+	});
+	
+	//Fetch 5 day forecast info in JSON format
+	var url = 'http://api.openweathermap.org/data/2.5/forecast/daily?lat=40.44062&lon=-79.995888&cnt=5&mode=json&appid=3aa8d2b200ad314410e9340650ad4140';
+	$.getJSON(url, function(weather){
+		var weatherToolTipHTML = "<table style=\"border:0px; color:black\">";
+		var datesHTML="";
+		var iconHTML="";
+		var tDayHTML="";
+		var tNightHTML="";
+		var d = new Date();
+		for(i in weather.list){
+			var futureDay = new Date(d.getYear(), d.getMonth(), d.getDate()+parseInt(i));
+			
+			datesHTML+="<th>"+(futureDay.getMonth()+1)+"/"+futureDay.getDate()+"</th>";
+			
+			var icon = 'http://openweathermap.org/img/w/'+weather.list[i].weather[0].icon+'.png';
+			iconHTML+= '<td><img src=\"' + icon + '\" style=\"\"></td>';
+			
+			var tDay = Math.round(1.8*(weather.list[i].temp.day-273.15)+32);
+			tDayHTML+= '<td><font style="font-size:1em;">'+tDay+'&deg</font></td>';
+			
+			var tNight = Math.round(1.8*(weather.list[i].temp.eve-273.15)+32);
+			tNightHTML+= '<td><font style="font-size:1em;">'+tNight+'&deg</font></td>';
+		}
+		weatherToolTipHTML+="<tr>"+datesHTML+"</tr><tr>"+iconHTML+"</tr><tr>"+tDayHTML+"</tr><tr>"+tNightHTML+"</tr></table>";
+		
+		$('#weather').tooltipster({
+			theme: 'tooltipster-light',
+	        content: $(weatherToolTipHTML),
+	        interactive: true
+	    });
 	});
 }
