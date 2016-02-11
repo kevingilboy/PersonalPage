b1fd3a833c6786a12bce8701045a9645b5a34205
Kevin Gilboy <kevinmgilboy@gmail.com>
2016-02-11 01:24:32 -0500

    Added tooltips for the stock ticker

--- stock.js
+++ stock.js
@@ -13,13 +13,28 @@ function getStocks() {
 	$.getJSON(url, function(stock){
 		//Format to ticker HTML
 		tickerHTML = '<marquee behavior="scroll" scrollamount="3" direction="left">';
+		tickerToolTipHTML = '<table class=\"tickerTooltip\" style=\"color:black;border-collapse:collapse;\">';
 		for(i in stock){
-			if(stock[i].c>=0)
+			if(stock[i].c>=0){
 				tickerHTML+=stock[i].t + " " + stock[i].l_cur + "(<font color=\"#70db70\">" + stock[i].c + "," + stock[i].cp + "%</font>)" + " | ";
-			else
+				tickerToolTipHTML += '<tr><td>'+stock[i].t+'</td><td>'+stock[i].l_cur+'</td><td><font color=\"#70db70\">' + stock[i].c + ' (' + stock[i].cp + '%)</font></td></tr>';
+			}
+			else{
 				tickerHTML+=stock[i].t + " " + stock[i].l_cur + "(<font color=\"#ff4d4d\">" + stock[i].c + "," + stock[i].cp + "%</font>)" + " | ";	
+				tickerToolTipHTML += '<tr><td>'+stock[i].t+'</td><td>'+stock[i].l_cur+'</td><td><font color=\"#ff4d4d\">' + stock[i].c + ' (' + stock[i].cp + '%)</font></td></tr>';
+			}
 		}
 		document.getElementById('ticker').innerHTML = tickerHTML+'</marquee>';
 		var t = setTimeout(getStocks, 300000);
+		
+		tickerToolTipHTML += '</table>';
+		$('#ticker').tooltipster({
+			theme: 'tooltipster-light',
+	        content: $(tickerToolTipHTML),
+	        interactive: true
+	    });
 	});
+	
+		
+
 }
